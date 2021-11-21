1d6fb0f2429c50875c487f9a257763be4dec9284
rnakai <rnakai@student.42tokyo.jp>
2021-11-21 17:48:03 +0900

    google testのsubmodule管理をやめて一つのブランチに統合

---
+++ googletest/1_5_0/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rnakai/42/container/github/googletest
+# Build directory: /home/rnakai/42/container/github/googletest/1_5_0
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(gtest_unittest "gtest_unittest")
+set_tests_properties(gtest_unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/rnakai/42/container/github/googletest/CMakeLists.txt;234;add_test;/home/rnakai/42/container/github/googletest/CMakeLists.txt;243;cxx_test_with_flags;/home/rnakai/42/container/github/googletest/CMakeLists.txt;247;cxx_test;/home/rnakai/42/container/github/googletest/CMakeLists.txt;0;")
