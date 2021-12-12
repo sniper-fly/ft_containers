eec1df7a96835bf8c81817fa2d4568388e60109d
rnakai <rnakai@student.42tokyo.jp>
2021-12-12 11:56:52 +0900

    gtestのデバッグシンボルつきバージョンとfsanitize addressつきをコンパイル

---
+++ googletest/1_5_0_debug/CMakeFiles/gtest_unittest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest_unittest.dir/test/gtest_unittest.cc.o"
+  "CMakeFiles/gtest_unittest.dir/test/gtest_unittest.cc.o.d"
+  "gtest_unittest"
+  "gtest_unittest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest_unittest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ googletest/1_5_0_fsanitize_address/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rnakai/42/container/github/googletest
+# Build directory: /home/rnakai/42/container/github/googletest/1_5_0_fsanitize_address
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(gtest_unittest "gtest_unittest")
+set_tests_properties(gtest_unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/rnakai/42/container/github/googletest/CMakeLists.txt;235;add_test;/home/rnakai/42/container/github/googletest/CMakeLists.txt;244;cxx_test_with_flags;/home/rnakai/42/container/github/googletest/CMakeLists.txt;248;cxx_test;/home/rnakai/42/container/github/googletest/CMakeLists.txt;0;")
