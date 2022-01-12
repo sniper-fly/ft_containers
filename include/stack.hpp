0b9279b87797422ab5eaf92b9980160f01549676
rnakai <rnakai@student.42tokyo.jp>
2022-01-12 17:59:27 +0900

    stackの実装（未テスト）

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,15 +1,63 @@
 #ifndef STACK_HPP
-# define STACK_HPP
+#define STACK_HPP
 
-class stack
+#include "vector.hpp"
+
+namespace ft
 {
-private:
+    template<class T, class Container = ft::vector<T> >
+    class stack
+    {
+    public:
+        typedef T         value_type;
+        typedef Container container_type;
+        typedef size_t    size_type;
 
-public:
-    stack();
-    virtual ~stack();
-    stack(stack const &other);
-    stack &operator=(stack const &other);
-};
+    protected:
+        container_type c;
+
+    public:
+        explicit stack(const container_type& ctnr = container_type())
+            : c(ctnr) {}
+        bool              empty() const { return c.empty(); }
+        size_type         size() const { return c.size(); }
+        value_type&       top() { return c.back(); }
+        const value_type& top() const { return c.back(); }
+        void              push(const value_type& val) { c.push_back(val); }
+        void              pop() { c.pop_back(); }
+    };
+
+    template<class T, class Container>
+    bool operator==(
+        const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+        return lhs.c == rhs.c;
+    }
+    template<class T, class Container>
+    bool operator!=(
+        const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+        return ! (lhs.c == rhs.c);
+    }
+    template<class T, class Container>
+    bool operator<(
+        const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+        return lhs.c < rhs.c;
+    }
+    template<class T, class Container>
+    bool operator<=(
+        const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+        return ! (rhs.c < lhs.c);
+    }
+    template<class T, class Container>
+    bool operator>(
+        const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+        return rhs.c < lhs.c;
+    }
+    template<class T, class Container>
+    bool operator>=(
+        const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+        return rhs.c <= lhs.c;
+    }
+
+} // namespace ft
 
 #endif
